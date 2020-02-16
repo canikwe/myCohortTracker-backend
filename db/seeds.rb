ccfeefba9985cb842e58e5515740fd723cc7458d
canikwe <c.anikwe@gmail.com>
2020-02-16 15:59:55 -0500

    add student cohort pairs models and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+
+cohort = Cohort.find_or_create_by(batch: 'dc-web-102819', name: '|dreams|', batch_id: 1157, current_mod: 6)
+
+10.times do
+  Student.find_or_create_by(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, cohort_id: cohort.id)
+end
+
+projects = ['SWAPI', 'Toy Tale', 'Hogwarts', 'Task Lister', 'OO Pets']
+
+10.times do
+  s1 = Student.all.sample
+  s2 = Student.all.sample
+  proj = projects.sample
+  Pair.find_or_create_by(s1_id: s1.id, s2_id: s2.id, mod: 1, name: proj, category: 'lab')
+  Pair.find_or_create_by(s1_id: s2.id, s2_id: s1.id, mod: 1, name: proj, category: 'lab')
+end
