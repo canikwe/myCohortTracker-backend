2841a536458693c18caa60cb4d71e5a5188e1298
canikwe <c.anikwe@gmail.com>
2020-02-22 15:06:24 -0500

    add group strong params and move student_ids to student_groups strong params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,8 @@ class GroupsController < ApplicationController
     # byebug
     activity = Activity.find_or_create_by(activity_params)
     group = Group.create!(activity_id: activity.id)
-    group_params[:student_ids].each do |id|
+    group.update(group_params)
+    student_group_params[:student_ids].each do |id|
       StudentGroup.find_or_create_by(student_id: id, group_id: group.id)
     end
 
@@ -18,11 +19,15 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:student_ids => [])
+    params.require(:group).permit(:avoid, :notes, :id, :activity_id)
   end
 
   def activity_params
     params.require(:activity).permit(:id, :name, :mod, :category)
+  end
+
+  def student_group_params
+    params.require(:student_group).permit(:student_ids => [])
   end
 
   def group_serializer
