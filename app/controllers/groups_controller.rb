10c44c91a35446836103b89ec58477b263cc3e67
canikwe <c.anikwe@gmail.com>
2020-02-22 16:52:51 -0500

    add update for groups and refactor group to student associations

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,25 +1,32 @@
 class GroupsController < ApplicationController
+  before_action :get_group, only: [:update, :delete]
 
   def index
     render json: Group.all.to_json(group_serializer)
   end
 
+  def update
+    @group.update(group_params)
+    
+    render json: @group.to_json(group_serializer)
+  end
+
   def create
-    # byebug
     activity = Activity.find_or_create_by(activity_params)
     group = Group.create!(activity_id: activity.id)
     group.update(group_params)
-    student_group_params[:student_ids].each do |id|
-      StudentGroup.find_or_create_by(student_id: id, group_id: group.id)
-    end
 
     render json: group.to_json(group_serializer)
   end
 
   private
 
+  def get_group
+    @group = Group.find(params[:id])
+  end
+
   def group_params
-    params.require(:group).permit(:avoid, :notes, :id, :activity_id)
+    params.require(:group).permit(:avoid, :notes, :id, :activity_id, :student_ids => [])
   end
 
   def activity_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :cohort_activities
   resources :activities, only: [:index, :create]
-  resources :groups, only: [:index, :create]
+  resources :groups, only: [:index, :create, :update]
   resources :students, only: [:index]
   resources :cohorts, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
