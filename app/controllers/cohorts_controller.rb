f8ba642d7afda933960a5e1e7e1764c06ffe0eb4
canikwe <c.anikwe@gmail.com>
2020-03-05 22:18:12 -0500

    add basc cohort create functionality

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -9,14 +9,28 @@ class CohortsController < ApplicationController
     render json: get_cohort
   end
 
+  def create
+    cohort = Cohort.create(cohort_params)
+    cohort.students.build(cohort_student_params[:students])
+    cohort.save
+    render json: cohort.to_json(serializer_options)
+  end
+
   private
+
+  def cohort_params
+    params.require(:cohort).permit(:batch, :name, :batch_id)
+  end
+
+  def cohort_student_params
+  params.require(:cohort).permit(:students => [:first_name, :last_name])
+  end
 
   def get_cohort
     Cohort.find(params[:id])
   end
 
   def serializer_options
-    { :except => [:created_at, :updated_at] }
-
+    { :except => [:created_at, :updated_at], :include => {:students => {:except => [:created_at, :updated_at]}} }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :activities, only: [:index, :create]
   resources :groups, except: [:new, :edit, :show]
   resources :students, only: [:index]
-  resources :cohorts, only: [:index, :show]
+  resources :cohorts, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
