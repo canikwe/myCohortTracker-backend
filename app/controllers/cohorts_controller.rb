bf871f7cca8c942366a64a171848a50eb663d60f
canikwe <c.anikwe@gmail.com>
2020-03-07 18:55:46 -0500

    cohort and student update working

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,5 +1,6 @@
 
 class CohortsController < ApplicationController
+  before_action :get_cohort, only: [:update]
 
   def index
     render json: Cohort.all.to_json(serializer_options)
@@ -15,7 +16,27 @@ class CohortsController < ApplicationController
     cohort = Cohort.create(cohort_params)
     cohort.students.create(cohort_student_params[:students])
 
-    render json: {cohort: cohort.as_json(serializer_options), students: cohort.students.as_json(serializer_options), groups: cohort.groups.as_json(group_serializer)}
+    render json: {cohort: cohort.as_json(serializer_options), students: cohort.students.as_json(serializer_options)}
+  end
+
+  def update
+    @cohort.update(cohort_params)
+    
+    student_ids = cohort_student_params[:students].map do |s_params|
+      
+      if s_params[:id]
+        student = Student.find(s_params[:id])
+        student.update(s_params)
+        s_params[:id]
+      else
+        student = @cohort.students.create(s_params)
+        student.id
+      end
+    end
+
+    @cohort.student_ids = student_ids
+    
+    render json: {cohort: @cohort.as_json(serializer_options), students: @cohort.students.as_json(serializer_options), groups: @cohort.groups.as_json(group_serializer)}
   end
 
   def csv_upload
@@ -30,7 +51,7 @@ class CohortsController < ApplicationController
     end
     cohort.students.create(students)
 
-    render json: {cohort: cohort.as_json(serializer_options), students: cohort.students.as_json(serializer_options), groups: cohort.groups.as_json(group_serializer)}
+    render json: {cohort: cohort.as_json(serializer_options), students: cohort.students.as_json(serializer_options)}
   end
 
   private
@@ -40,7 +61,7 @@ class CohortsController < ApplicationController
   end
 
   def cohort_student_params
-    params.require(:cohort).permit(:students => [:first_name, :last_name])
+    params.require(:cohort).permit(:students => [:first_name, :last_name, :cohort_id, :avatar, :id])
   end
 
   def get_cohort
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :activities, only: [:index, :create]
   resources :groups, except: [:new, :edit, :show]
   resources :students, only: [:index]
-  resources :cohorts, only: [:index, :show, :create]
+  resources :cohorts, except: [:new, :edit, :destroy]
   post '/cohorts/csv_upload', to: 'cohorts#csv_upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
