d4e9825ca1353e140f966df617cd691cfde8f1f1
canikwe <c.anikwe@gmail.com>
2020-03-08 16:35:02 -0400

    finish auth with before_actions working

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized?, only: [:create]
 
   def create
     instructor = Instructor.find_by(name: login_params[:name])
