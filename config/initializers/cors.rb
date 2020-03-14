a2abdbc5ab0efdaf944763f55270ac7989c3cc89
canikwe <c.anikwe@gmail.com>
2020-03-14 09:10:52 -0400

    update cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,16 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+    allow do
+    origins Rails.application.credentials.development_origin.to_s
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+    allow do
+    origins ENV['PRODUCTION_ORIGIN'].to_s
 
     resource '*',
       headers: :any,
