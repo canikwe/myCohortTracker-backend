5de36550229c7eb0e08061d0c65efc980ad9ee0f
canikwe <c.anikwe@gmail.com>
2020-06-14 16:53:54 -0400

    add student update route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,26 @@
 class StudentsController < ApplicationController
+  before_action :get_student
 
   def index
     render json: ApplicationSerializer.new(Student.all).as_json, status: :ok
+  end
+
+  def update
+    if @student.update(student_params)
+      render json: ApplicationSerializer.new(@student).as_json, status: :ok
+    else
+      render json: {message: @student.errors.full_messages.as_json}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def get_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:id, :first_name, :last_name, :cohort_id)
   end
 
 end
